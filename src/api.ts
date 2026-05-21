b30482d0195d7ec920ac468016fc62dcf3cd747e
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2026-05-21 00:53:22 -0400

    Use new image API for profile modal

--- src/api.ts
+++ src/api.ts
@@ -25,7 +25,7 @@ export async function fetchProfile(name: string): Promise<Profile | null> {
     const data = await response.json()
     return {
       name,
-      avatarUrl: data.avatar?.md || getAvatarUrl(name, 'md'),
+      avatarUrl: getAvatarUrl(name, 'md'),
       address: data.address,
       description: data.texts?.['description'] || '',
       url: data.texts?.['url'],
